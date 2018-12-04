1a0e268e13a8e32ecc64ede2f2ff9cbab0a6b2b9
giallon <nagiallombardo@gmail.com>
2018-12-04 12:17:58 +0100

    Add Home Fragment and add replace method fragment

--- app/src/main/java/fr/wildcodeschool/tpfragmentdrawer/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/tpfragmentdrawer/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity
 
     public void setFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.content_home, fragment);
+        transaction.replace(R.id.content_home, fragment);
         transaction.commit();
     }
 
