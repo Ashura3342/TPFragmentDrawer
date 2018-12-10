15657a1632135f03093c9e7a947b1b1bb12dfb55
giallon <nagiallombardo@gmail.com>
2018-12-10 11:52:41 +0100

    Map Navigator

--- app/src/main/java/fr/wildcodeschool/tpfragmentdrawer/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/tpfragmentdrawer/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.wildcodeschool.tpfragmentdrawer;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -9,47 +10,88 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import fr.wildcodeschool.tpfragmentdrawer.fragments.AboutFragment;
 import fr.wildcodeschool.tpfragmentdrawer.fragments.CategoryFragment;
 import fr.wildcodeschool.tpfragmentdrawer.fragments.HomeFragment;
-import fr.wildcodeschool.tpfragmentdrawer.fragments.ItemFragment;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    static class PageNavigation {
+        private int title;
+        private Class fragment;
+
+        PageNavigation(int title, Class fragment) {
+            this.title = title;
+            this.fragment = fragment;
+        }
+
+        public int getTitle() {
+            return title;
+        }
+
+        public Class getFragment() {
+            return fragment;
+        }
+
+        Fragment invoke() throws InstantiationException, IllegalAccessException {
+            return (Fragment) fragment.newInstance();
+        }
+    }
+
+
+    private static final SparseArray<PageNavigation> mapNavigation = new SparseArray<>();
+
+    static {
+        mapNavigation.put(R.id.nav_home, new PageNavigation(R.string.home_title, HomeFragment.class));
+        mapNavigation.put(R.id.nav_about, new PageNavigation(R.string.about_title, AboutFragment.class));
+        mapNavigation.put(R.id.nav_category, new PageNavigation(R.string.category_title, CategoryFragment.class));
+        mapNavigation.put(R.id.nav_item, new PageNavigation(R.string.item_title, HomeFragment.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
             this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        setFragment(new HomeFragment());
-
+        try {
+            navigationToPage(R.id.nav_home);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void setFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_home, fragment);
-        transaction.commit();
-        //add ne sert a rien autant mettre replace tout le temps????
+    public void navigationToPage(int id) throws IllegalAccessException, InstantiationException {
+        PageNavigation pageNavigation = mapNavigation.get(id);
+        if (pageNavigation != null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.content_home, pageNavigation.invoke());
+            transaction.commit();
+            setTitle(pageNavigation.getTitle());
+        }
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -81,25 +123,19 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            setFragment(new HomeFragment());
-            setTitle(R.string.home_title);
-        } else if (id == R.id.nav_category) {
-            setFragment(new CategoryFragment());
-            setTitle(R.string.category_title);
-        } else if (id == R.id.nav_item) {
-            setFragment(new ItemFragment());
-            setTitle(R.string.item_title);
-        } else if (id == R.id.nav_about) {
-            setFragment(new AboutFragment());
-            setTitle(R.string.about_title);
+        try {
+            navigationToPage(id);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
